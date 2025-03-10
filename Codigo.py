1ee27f8994d71f601dcb873e4e2b111822b88530
Jerith Vergara <jeritheli@gmail.com>
2025-03-09 19:30:08 -0500

    Segundo commit del avance del código del proyecto

--- Codigo.py
+++ Codigo.py
@@ -1,1 +1,3 @@
-print("Primera configuracion del proyecto, creacion del archivo de python")
+cantidad = int(input("Por favor ingrese la cantidad de piezas a comprar "))
+valor_unitario = float(input("Por favor ingrese el valor unitario de cada pieza "))
+total = (cantidad*valor_unitario)
