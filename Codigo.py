20a5c8e4d02998b674a39c05a47f51b96a08e741
Jerith Vergara <jeritheli@gmail.com>
2025-03-09 19:40:35 -0500

    Cuarto y ultimo avance del codigo del proyecto

--- Codigo.py
+++ Codigo.py
@@ -13,3 +13,11 @@ else:
 
 interes = (credito*0.20)
 totalcredito = (credito+interes)
+
+print(f"Resultados de los calculos:")
+print(f"Esta es la cantidad de las piezas a comprar: {cantidad}")
+print(f"Este es el precio unitario de cada pieza: ${valor_unitario:.2f}")
+print(f"El monto total de la compra es de ${total:.2f}")
+print(f"La inversión de la empresa es de ${inversion:.2f}")
+print(f"El préstamo al banco es de ${prestamo:.2f}")
+print(f"El crédito al fabricante con interés es de ${totalcredito:.2f}")
